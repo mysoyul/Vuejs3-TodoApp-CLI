e2a5c6f94f7b94467930fc2f1307af78966613f5
mysoyul <vega2k_@naver.com>
2023-12-06 16:03:58 +0900

    TodoList onBeforeMount add

--- src/components/TodoIList.vue
+++ src/components/TodoIList.vue
@@ -5,9 +5,20 @@
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+
+const todoItems = ref([])
+
+onBeforeMount(() => {
+    console.log('mounted in the composition api!')
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageValue = localStorage.key(i)
+            todoItems.value.push(storageValue)
+        }
+    }
+})
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
