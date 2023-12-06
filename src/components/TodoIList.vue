dee68a5e55922fb5aeff3a107ce96c9f40f1c3cc
mysoyul <vega2k_@naver.com>
2023-12-06 16:14:04 +0900

    TodoList removeTodo add

--- src/components/TodoIList.vue
+++ src/components/TodoIList.vue
@@ -23,42 +23,54 @@ onBeforeMount(() => {
     }
 })
 
+const removeTodo = (todoItem, index) => {
+    localStorage.removeItem(todoItem)
+    todoItems.value.splice(index, 1)
+}
+
+
 </script>
 
 <style scoped>
-i,span {
+i,
+span {
     cursor: pointer;
 }
+
 ul {
-  list-style-type: none;
-  padding-left: 0px;
-  margin-top: 0;
-  text-align: left;
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
 }
+
 li {
-  display: flex;
-  min-height: 50px;
-  height: 50px;
-  line-height: 50px;
-  margin: 0.5rem 0;
-  padding: 0 0.9rem;
-  background: white;
-  border-radius: 5px;
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
 }
+
 .removeBtn {
-  margin-left: auto;
-  color: #de4343;
+    margin-left: auto;
+    color: #de4343;
 }
+
 .checkBtn {
-  line-height: 45px;
-  color: #62acde;
-  margin-right: 5px;
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
 }
+
 .checkBtnCompleted {
-  color: #b3adad;
+    color: #b3adad;
 }
+
 .textCompleted {
-  text-decoration: line-through;
-  color: #b3adad;
-}
-</style>
+    text-decoration: line-through;
+    color: #b3adad;
+}</style>
