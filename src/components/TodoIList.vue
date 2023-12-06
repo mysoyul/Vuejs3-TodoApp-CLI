da3b69c9c4c0ffe64d81fe00b0c4f2a1239b0581
mysoyul <vega2k_@naver.com>
2023-12-06 16:08:01 +0900

    TodoList style add

--- src/components/TodoIList.vue
+++ src/components/TodoIList.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        List
+        <ul>
+            <li v-for="(item, index) in todoItems" :key="index">
+                {{ item }}
+            </li>
+        </ul>
+
     </div>
 </template>
 
@@ -10,7 +15,6 @@ import { ref, onBeforeMount } from 'vue'
 const todoItems = ref([])
 
 onBeforeMount(() => {
-    console.log('mounted in the composition api!')
     if (localStorage.length > 0) {
         for (var i = 0; i < localStorage.length; i++) {
             const storageValue = localStorage.key(i)
@@ -21,4 +25,37 @@ onBeforeMount(() => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
