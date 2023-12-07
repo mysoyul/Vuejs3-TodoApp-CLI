1f69b141db968a1f816488801457b9aaf977e233
mysoyul <vega2k_@naver.com>
2023-12-07 13:22:44 +0900

    store에 loadTodoItems() 함수추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,51 @@
 import { createStore, createLogger } from "vuex"
 import http from "@/common/http-common"
+import axios from "axios"
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                const itemJson = localStorage.getItem(localStorage.key(i))
-                if (itemJson) {
-                    arr.push(JSON.parse(itemJson))
-                }
-            }
-        }
-        return arr;
-    }
-}
+// const storage = {
+//     fetch() {
+//         const arr = [];
+//         if (localStorage.length > 0) {
+//             for (let i = 0; i < localStorage.length; i++) {
+//                 const itemJson = localStorage.getItem(localStorage.key(i))
+//                 if (itemJson) {
+//                     arr.push(JSON.parse(itemJson))
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
     state: {
-        todoItems: storage.fetch()
+        todoItems: []
     },
     actions: {
-
+        loadTodoItems({ commit }) {
+            http
+                .get('/todos')
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+                .catch(error => {
+                    if (axios.isAxiosError(error)) {
+                        console.log(error?.response?.status +
+                            ' : ' + error.message)
+                    } else {
+                        console.error(error);
+                        throw error;
+                    }
+                });
+        },
     },
     mutations: {
+        setTodoItems(state, items) {
+            state.todoItems = items;
+        },
         addTodo(state, todoItem) {
             const obj = { completed: false, item: todoItem };
             localStorage.setItem(todoItem, JSON.stringify(obj));
