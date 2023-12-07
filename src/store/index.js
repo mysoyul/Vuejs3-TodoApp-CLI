39d3711bfae77155c1158ec2485bcb4a12f8d96d
mysoyul <vega2k_@naver.com>
2023-12-07 15:32:51 +0900

    modulePost add

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, createLogger } from "vuex"
 import { moduleTodo} from '@/store/modules/moduleTodo'
-import { modulePost } from './modules/modulePost'
+import { modulePost } from '@/store/modules/modulePost'
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
